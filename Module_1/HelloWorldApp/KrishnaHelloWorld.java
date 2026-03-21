6016b039887636c7a58c173d927a2b934655eae3
athira <asuriyakrishna@my365.bellevue.edu>
2026-03-21 15:39:06 -0500

    init

---
+++ Module_1/HelloWorldApp/KrishnaHelloWorld.java
@@ -0,0 +1,11 @@
+public class KrishnaHelloWorld {
+    public static void myName(String fName, String lName) {
+        System.out.println("Hello World from " + fName + " " + lName);
+    }
+
+    public static void main(String[] args) {
+        String fName = "Athira";
+        String lName = "Krishna";
+        myName(fName, lName);
+    }
+}
